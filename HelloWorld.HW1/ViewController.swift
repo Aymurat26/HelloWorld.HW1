36258743c791cf6a0bf6956b452dd892a8f0cf6d
Aymurat <aymurat@icloud.com>
2023-01-28 19:27:44 +0300

    Initial Commit

---
+++ HelloWorld.HW1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld.HW1
+//
+//  Created by Aymurat on 28.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
